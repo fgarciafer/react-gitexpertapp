c9839c47edc7b12960164b994e18a8319f2f2184
Felipe <felipe.garcia.fernandez@gmail.com>
2020-10-08 16:27:46 +0200

    docs agregados

---
+++ docs/precache-manifest.4987622cdd8d462ecbf20579cd831227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf35f044174e0845dcb11c0cfd37d7cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5a3d73fdb02434582f",
+    "url": "/static/css/main.a337b2f3.chunk.css"
+  },
+  {
+    "revision": "ec6e3ac8caeb08d62c89",
+    "url": "/static/js/2.cd2e7254.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd2e7254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5a3d73fdb02434582f",
+    "url": "/static/js/main.9ac813cf.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
